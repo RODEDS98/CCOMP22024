4d152549f9bcf32938f43c80b2ecd776565e0ce3
ALUMNOP-104 <ALUMNOP-104@UCSP.AULAB>
2024-04-04 08:32:09 -0500

    date 2

--- examenes/Date.h
+++ examenes/Date.h
@@ -14,7 +14,38 @@ class Date{
                 year =1900;
             }
             if (d >=1 && d <=31)
-                month =m;
+                {
+                if (isLeapYear() && month =2){
+                    do {
+                        std::cout<<"ingrese un dia valido para el mes de febrero en año viciesto"<<std::endl;                
+                    }
+                    while (!(d >=1 && d <=29));
+                        day =y; 
+                    }
+                else if (!(isLeapYear()) && month =2){
+                    do {
+                        std::cout<<"ingrese un dia valido para el mes de febrero "<<std::endl;                
+                    }
+                    while (!(d >=1 && d <=28));
+                        day =y; 
+                    }
+                else
+                    if ((month =4 || month =6 || month =9 || month =11 ) && (day  <=31 || day <=1 )){
+
+                    day =y;
+                    else{
+                        do {
+                        std::cout<<"ingrese un dia valido para un mes de solo 30 dias "<<std::endl;                
+                        }
+                        while (!(d >=1 && d <=30));
+                        day =y;
+
+                    }
+                }     
+                else
+                    day =y;
+                }
+
             
         }
         void setDay(int d){
