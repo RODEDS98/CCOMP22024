4e1a52e0c817eeb30ea640dcfa26e33030ccf873
ALUMNOP-104 <ALUMNOP-104@UCSP.AULAB>
2024-04-04 08:01:31 -0500

    2024

---
+++ extras/suma/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout << "ingresa 2 numeros : " << endl;
+    cin >> a>>b;
+    cout << "a+b : " << a+b;
+
+    return 0;
+    // hola :v :v
+    // buscas el main.cpp del codigo
+    // git add main.cpp
+    // git commit -m "COMENTARIO"
+    // git push -u origin master
+}
